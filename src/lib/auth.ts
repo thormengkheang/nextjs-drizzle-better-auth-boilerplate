044cc1e127c03ea0a6a4b03fbf3588fb4923012c
Mengkheang Thor <thormengkheang@gmail.com>
2025-06-26 17:09:12 +0700

    add schema to auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,11 +3,16 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
 import { admin, username } from "better-auth/plugins";
 import { nextCookies } from "better-auth/next-js";
+import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
   enableEmailVerification: true,
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema: {
+      ...schema,
+      user: schema.user,
+    },
   }),
   plugins: [nextCookies(), username(), admin()],
 });
